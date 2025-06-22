64f7f0d71e4e535024de42e90f34582e00ecf48a
ROCKOIPENG <165926841+ROCKOIPENG@users.noreply.github.com>
2025-06-23 00:16:35 +0800

    Delete Project/AIR_Conditioner/AIR directory

--- Project/AIR_Conditioner/AIR/min.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?30c8bd8b9d6f35d97c76dc27ca93358f";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
